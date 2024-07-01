b51eb57a5dc6e8b59ab9d29d4012fef40659a4fc
Arti <z.artemznahar@gmail.com>
2024-07-02 02:05:12 +0300

    Fix: output

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const { processFees } = require('./fees/index');
     const data = await readFromJSON('example.json');
     if (data) {
         if (Array.isArray(data)) {
-            const feesReduced = processFees(data)
-            console.log(feesReduced)
+            const feesReduced = processFees(data) // string[]
+            feesReduced.forEach((i) => {
+                console.log(i)
+            })
         }
     }
 })();
